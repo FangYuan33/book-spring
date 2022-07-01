4f6cf0e7701bd7bedb8f2757557d9c6c9bd9ad12
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2022-07-01 20:54:05 +0800

    ioc easy: 添加bean缓存

--- src/main/java/framework/spring/factory/BeanFactory.java
+++ src/main/java/framework/spring/factory/BeanFactory.java
@@ -1,12 +1,16 @@
 package framework.spring.factory;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 /**
  * Bean的静态工厂
  */
 public class BeanFactory {
+
+    private static final Map<String, Object> beanCache = new HashMap<>();
 
     private static final Properties BEAN_CONFIG;
 
@@ -22,11 +26,22 @@ public class BeanFactory {
 
     public static Object getBean(String beanName) {
         try {
-            return Class.forName(BEAN_CONFIG.getProperty(beanName)).newInstance();
+            if (!beanCache.containsKey(beanName)) {
+                synchronized (BeanFactory.class) {
+                    if (!beanCache.containsKey(beanName)) {
+                        Object bean = Class.forName(BEAN_CONFIG.getProperty(beanName)).newInstance();
+                        beanCache.put(beanName, bean);
+
+                        return bean;
+                    }
+                }
+            }
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("BeanFactory have not [" + beanName + "] bean!", e);
         } catch (InstantiationException | IllegalAccessException e) {
             throw new RuntimeException("[" + beanName + "] instantiation error!", e);
         }
+
+        return beanCache.get(beanName);
     }
 }
