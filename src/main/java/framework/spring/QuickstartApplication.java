9b9c79e270017a6cc3addfaae7960f98cce524df
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2022-07-02 20:45:47 +0800

    ioc easy: 依赖查找DL ByClass

--- src/main/java/framework/spring/QuickstartApplication.java
+++ src/main/java/framework/spring/QuickstartApplication.java
@@ -1,6 +1,7 @@
 package framework.spring;
 
 import framework.spring.basic_dl.Person;
+import framework.spring.dao.DemoDao;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,8 +9,15 @@ public class QuickstartApplication {
     public static void main(String[] args) {
         BeanFactory factory = new ClassPathXmlApplicationContext("basic_dl/quickstart-byname.xml");
 
-        Person person = (Person) factory.getBean("person");
+        // byName
+        Person personByName = (Person) factory.getBean("person");
+        System.out.println(personByName);
 
-        System.out.println(person);
+        // byClass
+        Person personByClass = factory.getBean(Person.class);
+        System.out.println(personByClass);
+
+        DemoDao demoDao = factory.getBean(DemoDao.class);
+        System.out.println(demoDao.findAll());
     }
 }
