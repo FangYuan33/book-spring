34bef6a8be6507c1716fdf826e1a4a5dbe4c840c
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2022-07-21 22:03:37 +0800

    aop easy: servlet创建jdk动态代理

--- src/main/java/framework/spring/servlet/DemoServlet.java
+++ src/main/java/framework/spring/servlet/DemoServlet.java
@@ -3,23 +3,54 @@ package framework.spring.servlet;
 import framework.spring.decorator.DemoServiceDecorator;
 import framework.spring.factory.BeanFactory;
 import framework.spring.service.DemoService;
+import framework.spring.utils.LogUtils;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.Proxy;
 
 @WebServlet(urlPatterns = "/demo")
 public class DemoServlet extends HttpServlet {
 
+    private DemoService demoService;
+
     private DemoService decoratorDemoService = new DemoServiceDecorator((DemoService) BeanFactory.getBean("demoService"));
 
     private DemoService abstractMethodDemoService = (DemoService) BeanFactory.getBean("demoService");
+
+    @Override
+    public void init() {
+        jdkProxy();
+    }
+
+    /**
+     * jdk动态代理默认给所有的方法都增强
+     */
+    private void jdkProxy() {
+        DemoService demoService = (DemoService) BeanFactory.getBean("demoService");
+        Class<? extends DemoService> demoServiceClass = demoService.getClass();
+
+        // jdk动态代理
+        this.demoService = (DemoService) Proxy
+                .newProxyInstance(demoServiceClass.getClassLoader(), demoServiceClass.getInterfaces(),
+                        ((proxy, method, args) -> {
+                            // 条件增强方法
+                            if ("add".equals(method.getName())) {
+                                LogUtils.printLog(demoServiceClass.getName(), method.getName(), args);
+                            }
+
+                            return method.invoke(demoService, args);
+                        }));
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.getWriter().println(abstractMethodDemoService.findAll().toString());
+        resp.getWriter().println(demoService.findAll().toString());
 
-        abstractMethodDemoService.add("FYuan", 33);
+        demoService.add("FYuan", 33);
     }
 }
