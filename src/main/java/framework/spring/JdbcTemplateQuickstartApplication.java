1690dc228aa4adc9468687d2defb1a51632ce43f
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2022-07-27 22:15:01 +0800

    spring dao: jdbcTemplate select ok

--- src/main/java/framework/spring/JdbcTemplateQuickstartApplication.java
+++ src/main/java/framework/spring/JdbcTemplateQuickstartApplication.java
@@ -1,15 +1,25 @@
 package framework.spring;
 
 import framework.spring.config.DataSourceConfig;
+import framework.spring.pojo.User;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
 
 public class JdbcTemplateQuickstartApplication {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DataSourceConfig.class);
-
         JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
 
-        jdbcTemplate.execute("insert into tbl_user (name, tel) values ('FangYuan', '12345')");
+        initialJdbcTemplate(jdbcTemplate);
+    }
+
+    private static void initialJdbcTemplate(JdbcTemplate jdbcTemplate) {
+//        jdbcTemplate.execute("insert into tbl_user (name, tel) values ('FangYuan', '12345')");
+
+        List<User> users = jdbcTemplate.query("select * from tbl_user", new BeanPropertyRowMapper<>(User.class));
+        users.forEach(System.out::println);
     }
 }
---
+++ src/main/java/framework/spring/pojo/User.java
@@ -0,0 +1,13 @@
+package framework.spring.pojo;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+    private String tel;
+}
