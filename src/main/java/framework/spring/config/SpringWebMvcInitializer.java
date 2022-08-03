7617fd648f2b7bfe719cd82d7f82d40153046d46
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2022-08-03 15:25:18 +0800

    spring mvc: mvc 注解配置

---
+++ src/main/java/framework/spring/config/SpringWebMvcInitializer.java
@@ -0,0 +1,23 @@
+package framework.spring.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * 配置 DispatcherServlet
+ */
+public class SpringWebMvcInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {RootConfiguration.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {WebMvcConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
